443879afb36223c3d68b38830269bef692b02501
t-kuni <t.kuni.dev@gmail.com>
2020-04-30 19:14:37 +0900

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel v2
                 </div>
 
                 <div class="links">
